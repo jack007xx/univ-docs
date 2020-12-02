779f5f18cfa740318a4f4d4f301a6e5baea894e0
jack007xx <jack007xx@gmail.com>
2020-12-03 04:25:54 +0900

    9書いた

--- 3rd/autumn/CS_experiment2/submissions/03/src/09/mlut.c
+++ 3rd/autumn/CS_experiment2/submissions/03/src/09/mlut.c
@@ -80,149 +80,22 @@ main() {
 /* unsigned int result を返す */
 unsigned int my_a2i(str) unsigned int *str;
 {
-  unsigned int *str_tmp;
-  unsigned int k;
-  unsigned int result;
-
-  str_tmp = str;
-  for (k = 0; *str_tmp != '\0'; k++) {
-    str_tmp++;
-  }
-
-  result = 0;
-  str_tmp = str;
-
-  if (k == 1) {
-    result = *str_tmp - '0';
-  } else if (k == 2) {
-    for (k = 0; k < (*str_tmp - '0'); k++) {
-      result = result + 10;
-    }
-    str_tmp++;
-    result = result + (*str_tmp - '0');
-  } else if (k == 3) {
-    for (k = 0; k < (*str_tmp - '0'); k++) {
-      result = result + 100;
-    }
-    str_tmp++;
-    for (k = 0; k < (*str_tmp - '0'); k++) {
-      result = result + 10;
-    }
-    str_tmp++;
-    result = result + (*str_tmp - '0');
-  }
-
-  return result;
+  // 省略
 }
 
 /* unsigned int i を文字列（数字）に変換して print する関数 */
 void my_i2a(unsigned int i) {
-  unsigned int counter;
-  unsigned int s[4];
-
-  for (counter = 0; i >= 10; counter++) {
-    i -= 10;
-  }
-  s[0] = counter + '0';
-  s[1] = i + '0';
-  s[2] = ' ';
-  s[3] = '\0';
-
-  my_print(s);
+  // 省略
 }
 
 /* キーボードから入力された文字列を str[] に記憶する関数 */
 void my_scan(str) unsigned int *str;
 {
-  EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
-  EXTIO_SCAN_REQ = (unsigned int)0x00000001;
-  EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
-
-  EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
-  EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
-  while (EXTIO_SCAN_ASCII == SCAN_STRORING) {
-    EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
-    EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
-  }
-
-  while ((*str = EXTIO_SCAN_ASCII) != (unsigned int)0x3e) {  // 0x3e=RETURN
-    if ((*str >= 1) && (*str <= 26)) {
-      *str = 'A' + *str - 1;
-    } else if ((*str >= 48) && (*str <= 57)) {
-      *str = '0' + *str - 48;
-    } else {
-      if (*str == 0) {
-        *str = '@';
-      } else if (*str == 27) {
-        *str = '[';
-      } else if (*str == 29) {
-        *str = ']';
-      } else if ((*str >= 32) && (*str <= 47)) {
-        *str = ' ' + *str - 32;
-      } else if (*str == 58) {
-        *str = '?';
-      } else if (*str == 59) {
-        *str = '=';
-      } else if (*str == 60) {
-        *str = ';';
-      } else if (*str == 61) {
-        *str = ':';
-      } else if (*str == 62) {
-        *str = '\n';
-      } else {
-        *str = '@';
-      }
-    }
-    EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
-    EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
-    str++;
-  }
-  *str = '\0';
-
-  EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
-  EXTIO_SCAN_REQ = (unsigned int)0x00000000;
-  EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
-
-  EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
+  // 省略
 }
 
 /* 文字列 str[] を表示する関数 */
 void my_print(str) unsigned int *str;
 {
-  while (*str != '\0') {
-    EXTIO_PRINT_STROKE = (unsigned int)0x00000000;
-
-    if ((*str >= 'A') && (*str <= 'Z')) {
-      EXTIO_PRINT_ASCII = *str - 'A' + 1;
-    } else if ((*str >= 'a') && (*str <= 'z')) {
-      EXTIO_PRINT_ASCII = *str - 'a' + 1;
-    } else if ((*str >= '0') && (*str <= '9')) {
-      EXTIO_PRINT_ASCII = *str - '0' + 48;
-    } else {
-      if (*str == '@') {
-        EXTIO_PRINT_ASCII = (unsigned int)0;
-      } else if (*str == '[') {
-        EXTIO_PRINT_ASCII = (unsigned int)27;
-      } else if (*str == ']') {
-        EXTIO_PRINT_ASCII = (unsigned int)29;
-      } else if ((*str >= ' ') && (*str <= '/')) {
-        EXTIO_PRINT_ASCII = *str - ' ' + 32;
-      } else if (*str == '?') {
-        EXTIO_PRINT_ASCII = (unsigned int)58;
-      } else if (*str == '=') {
-        EXTIO_PRINT_ASCII = (unsigned int)59;
-      } else if (*str == ';') {
-        EXTIO_PRINT_ASCII = (unsigned int)60;
-      } else if (*str == ':') {
-        EXTIO_PRINT_ASCII = (unsigned int)61;
-      } else if (*str == '\n') {
-        EXTIO_PRINT_ASCII = (unsigned int)62;
-      } else {
-        EXTIO_PRINT_ASCII = (unsigned int)0x00000000;
-      }
-    }
-
-    EXTIO_PRINT_STROKE = (unsigned int)0x00000001;
-    str++;
-  }
+  // 省略
 }
