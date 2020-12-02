08529bfd911a2310e9b80f3e68e6c719786c2b55
jack007xx <jack007xx@gmail.com>
2020-12-02 23:56:47 +0900

    6-2を書いた

--- 3rd/autumn/CS_experiment2/submissions/03/src/06/my_scan.c
+++ 3rd/autumn/CS_experiment2/submissions/03/src/06/my_scan.c
@@ -4,14 +4,13 @@
 
 #define SCAN_STRORING (unsigned int)0xffffffff
 
-#define EXTIO_PRINT_STROKE (*(volatile unsigned int *) 0x0300)
-#define EXTIO_PRINT_ASCII  (*(volatile unsigned int *) 0x0304)
+#define EXTIO_PRINT_STROKE (*(volatile unsigned int *)0x0300)
+#define EXTIO_PRINT_ASCII (*(volatile unsigned int *)0x0304)
 
 void my_print();
 void my_scan();
 
-main()
-{
+main() {
   unsigned int string1[32];
   unsigned int string2[32];
 
@@ -26,7 +25,7 @@ main()
   string1[8] = '\0';
 
   my_print(string1);
-  
+
   while (1) {
     string1[0] = 'S';
     string1[1] = 'T';
@@ -36,11 +35,11 @@ main()
     string1[5] = 'G';
     string1[6] = '=';
     string1[7] = '\0';
-    
+
     my_print(string1);
-    
+
     my_scan(string2);
-    
+
     string1[0] = 'E';
     string1[1] = 'C';
     string1[2] = 'H';
@@ -49,7 +48,7 @@ main()
     string1[5] = '\0';
 
     my_print(string1);
-    
+
     my_print(string2);
 
     string1[0] = '\n';
@@ -59,63 +58,61 @@ main()
   }
 }
 
-void my_scan(str)
-     unsigned int *str;
+void my_scan(str) unsigned int *str;
 {
-    EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
-    EXTIO_SCAN_REQ = (unsigned int)0x00000001;
-    EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
+  EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
+  EXTIO_SCAN_REQ = (unsigned int)0x00000001;
+  EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
 
+  EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
+  EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
+  while (EXTIO_SCAN_ASCII == SCAN_STRORING) {
     EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
     EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
-    while (EXTIO_SCAN_ASCII == SCAN_STRORING) {
-      EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
-      EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
-    }
+  }
 
-    while ((*str = EXTIO_SCAN_ASCII) != (unsigned int)0x3e) {    // 0x3e=RETURN
-      if ((*str >= 1) && (*str <= 26)) {
-	*str = 'A' + *str - 1;
-      } else if ((*str >= 48) && (*str <= 57)) {
-	*str = '0' + *str - 48;
+  while ((*str = EXTIO_SCAN_ASCII) != (unsigned int)0x3e) {  // 0x3e=RETURN
+    if ((*str >= 1) && (*str <= 26)) {
+      *str = 'A' + *str - 1;
+    } else if ((*str >= 48) && (*str <= 57)) {
+      *str = '0' + *str - 48;
+    } else {
+      if (*str == 0) {
+        *str = '@';
+      } else if (*str == 27) {
+        *str = '[';
+      } else if (*str == 29) {
+        *str = ']';
+      } else if ((*str >= 32) && (*str <= 47)) {
+        *str = ' ' + *str - 32;
+      } else if (*str == 58) {
+        *str = '?';
+      } else if (*str == 59) {
+        *str = '=';
+      } else if (*str == 60) {
+        *str = ';';
+      } else if (*str == 61) {
+        *str = ':';
+      } else if (*str == 62) {
+        *str = '\n';
       } else {
-	if (*str == 0) {
-	  *str = '@';
-	} else if (*str == 27) {
-	  *str = '[';
-	} else if (*str == 29) {
-	  *str = ']';
-	} else if ((*str >= 32) && (*str <= 47)) {
-	  *str = ' ' + *str - 32;
-	} else if (*str == 58) {
-	  *str = '?';
-	} else if (*str == 59) {
-	  *str = '=';
-	} else if (*str == 60) {
-	  *str = ';';
-	} else if (*str == 61) {
-	  *str = ':';
-	} else if (*str == 62) {
-	  *str = '\n';
-	} else {
-	  *str = '@';
-	}
+        *str = '@';
       }
-      EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
-      EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
-      str++;
     }
-    *str = '\0';
-
     EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
-    EXTIO_SCAN_REQ = (unsigned int)0x00000000;
     EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
+    str++;
+  }
+  *str = '\0';
 
-    EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
+  EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
+  EXTIO_SCAN_REQ = (unsigned int)0x00000000;
+  EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
+
+  EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
 }
 
-void my_print(str)
-     unsigned int *str;
+void my_print(str) unsigned int *str;
 {
   while (*str != '\0') {
     EXTIO_PRINT_STROKE = (unsigned int)0x00000000;
@@ -128,28 +125,28 @@ void my_print(str)
       EXTIO_PRINT_ASCII = *str - '0' + 48;
     } else {
       if (*str == '@') {
-	EXTIO_PRINT_ASCII = (unsigned int)0;
+        EXTIO_PRINT_ASCII = (unsigned int)0;
       } else if (*str == '[') {
-	EXTIO_PRINT_ASCII = (unsigned int)27;
+        EXTIO_PRINT_ASCII = (unsigned int)27;
       } else if (*str == ']') {
-	EXTIO_PRINT_ASCII = (unsigned int)29;
+        EXTIO_PRINT_ASCII = (unsigned int)29;
       } else if ((*str >= ' ') && (*str <= '/')) {
-	EXTIO_PRINT_ASCII = *str - ' ' + 32;
+        EXTIO_PRINT_ASCII = *str - ' ' + 32;
       } else if (*str == '?') {
-	EXTIO_PRINT_ASCII = (unsigned int)58;
+        EXTIO_PRINT_ASCII = (unsigned int)58;
       } else if (*str == '=') {
-	EXTIO_PRINT_ASCII = (unsigned int)59;
+        EXTIO_PRINT_ASCII = (unsigned int)59;
       } else if (*str == ';') {
-	EXTIO_PRINT_ASCII = (unsigned int)60;
+        EXTIO_PRINT_ASCII = (unsigned int)60;
       } else if (*str == ':') {
-	EXTIO_PRINT_ASCII = (unsigned int)61;
+        EXTIO_PRINT_ASCII = (unsigned int)61;
       } else if (*str == '\n') {
-	EXTIO_PRINT_ASCII = (unsigned int)62;
+        EXTIO_PRINT_ASCII = (unsigned int)62;
       } else {
-	EXTIO_PRINT_ASCII = (unsigned int)0x00000000;
+        EXTIO_PRINT_ASCII = (unsigned int)0x00000000;
       }
     }
-    
+
     EXTIO_PRINT_STROKE = (unsigned int)0x00000001;
     str++;
   }
