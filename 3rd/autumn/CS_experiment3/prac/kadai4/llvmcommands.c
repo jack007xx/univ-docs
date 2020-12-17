0ae4a6d8f10aca5f0659d6f98d22de083d37ece9
jack007xx <jack007xx@gmail.com>
2020-12-17 09:56:17 +0900

    フォーマット、factorpushの引数変更

--- 3rd/autumn/CS_experiment3/prac/kadai4/llvmcommands.c
+++ 3rd/autumn/CS_experiment3/prac/kadai4/llvmcommands.c
@@ -1,38 +1,45 @@
-#include <stdlib.h>
 #include "llvmcommands.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 LLVMcode *codehd = NULL; /* 命令列の先頭のアドレスを保持するポインタ */
 LLVMcode *codetl = NULL; /* 命令列の末尾のアドレスを保持するポインタ */
-Fundecl *declhd = NULL; /* 関数定義の線形リストの先頭の要素のアドレスを保持するポインタ */
-Fundecl *decltl = NULL; /* 関数定義の線形リストの末尾の要素のアドレスを保持するポインタ */
+Fundecl *declhd =
+    NULL; /* 関数定義の線形リストの先頭の要素のアドレスを保持するポインタ */
+Fundecl *decltl =
+    NULL; /* 関数定義の線形リストの末尾の要素のアドレスを保持するポインタ */
 
-Factorstack fstack; /* 整数もしくはレジスタ番号を保持するスタック */
+Factorstack FSTACK; /* 整数もしくはレジスタ番号を保持するスタック */
 
-void init_fstack(){ /* fstackの初期化 */
-  fstack.top = 0;
+void init_fstack() { /* FSTACKの初期化 */
+  FSTACK.top = 0;
   return;
 }
 
 Factor fuctorpop() {
   Factor tmp;
-  tmp = fstack.element[fstack.top];
-  fstack.top --;
+  tmp = FSTACK.element[FSTACK.top];
+  FSTACK.top--;
   return tmp;
 }
 
-void fuctorpush(Factor x) {
-  fstack.top ++;
-  fstack.element[fstack.top] = x;
+void fuctorpush(char *aName, int aVal, Scope aType) {
+  FSTACK.top++;
+  strcpy(FSTACK.element[FSTACK.top].vname, aName);
+  FSTACK.element[FSTACK.top].val = aVal;
+  FSTACK.element[FSTACK.top].type = aType;
   return;
 }
 
-void add_code(LLVMCode* tmp){
-  if( codetl == NULL ){ /* 解析中の関数の最初の命令の場合 */
-    if( decltl == NULL ){   /* 解析中の関数がない場合 */
+void add_code(LLVMcode *tmp) {
+  if (codetl == NULL) {   /* 解析中の関数の最初の命令の場合 */
+    if (decltl == NULL) { /* 解析中の関数がない場合 */
       /* 関数宣言を処理する段階でリストが作られているので，ありえない */
-      fprintf(stderr,"unexpected error\n");
+      fprintf(stderr, "unexpected error\n");
     }
-    decltl->codes = tmp;   /* 関数定義の命令列の先頭の命令に設定 */
+    decltl->codes = tmp; /* 関数定義の命令列の先頭の命令に設定 */
     codehd = codetl = tmp; /* 生成中の命令列の末尾の命令として記憶 */
   } else { /* 解析中の関数の命令列に1つ以上命令が存在する場合 */
     codetl->next = tmp; /* 命令列の末尾に追加 */
--- 3rd/autumn/CS_experiment3/prac/kadai4/llvmcommands.h
+++ 3rd/autumn/CS_experiment3/prac/kadai4/llvmcommands.h
@@ -1,7 +1,8 @@
 #ifndef __LLVMCOMMANDS_H__
 #define __LLVMCOMMANDS_H__
+#include "symtab.h"
 /* LLVM命令名の定義 */
-typedef enum { 
+typedef enum {
   Alloca,   /* alloca */
   Store,    /* store  */
   Load,     /* load   */
@@ -15,7 +16,7 @@ typedef enum {
 } LLVMcommand;
 
 /* 比較演算子の種類 */
-typedef enum { 
+typedef enum {
   EQUAL, /* eq （==）*/
   NE,    /* ne （!=）*/
   SGT,   /* sgt （>，符号付き） */
@@ -26,33 +27,44 @@ typedef enum {
 
 typedef struct llvmcode {
   LLVMcommand command; /* 命令名 */
-  union { /* 命令の引数 */
-    struct { /* alloca */
+  union {              /* 命令の引数 */
+    struct {           /* alloca */
       Factor retval;
     } alloca;
     struct { /* store  */
-      Factor arg1;  Factor arg2;
+      Factor arg1;
+      Factor arg2;
     } store;
     struct { /* load   */
-      Factor arg1;  Factor retval;
+      Factor arg1;
+      Factor retval;
     } load;
     struct { /* br     */
       int arg1;
     } bruncond;
     struct { /* brc    */
-      Factor arg1;  int arg2;  int arg3;
+      Factor arg1;
+      int arg2;
+      int arg3;
     } brcond;
     struct { /* label  */
       int l;
     } label;
     struct { /* add    */
-      Factor arg1;  Factor arg2;  Factor retval;
+      Factor arg1;
+      Factor arg2;
+      Factor retval;
     } add;
     struct { /* sub    */
-      Factor arg1;  Factor arg2;  Factor retval;
+      Factor arg1;
+      Factor arg2;
+      Factor retval;
     } sub;
     struct { /* icmp   */
-      Cmptype type;  Factor arg1;  Factor arg2;  Factor retval;
+      Cmptype type;
+      Factor arg1;
+      Factor arg2;
+      Factor retval;
     } icmp;
     struct { /* ret    */
       Factor arg1;
@@ -63,31 +75,32 @@ typedef struct llvmcode {
 } LLVMcode;
 
 /* 変数もしくは定数の型 */
+// Row型に合わせて順番変更
 typedef struct {
-  Scope type;      /* 変数（のレジスタ）か整数の区別 */
   char vname[256]; /* 変数の場合の変数名 */
-  int val;         /* 整数の場合はその値，変数の場合は割り当てたレジスタ番号 */
+  int val; /* 整数の場合はその値，変数の場合は割り当てたレジスタ番号 */
+  Scope type; /* 変数（のレジスタ）か整数の区別 */
 } Factor;
 
 /* 変数もしくは定数のためのスタック */
 typedef struct {
-  Factor element[100];  /* スタック（最大要素数は100まで） */
-  unsigned int top;     /* スタックのトップの位置         */
+  Factor element[100]; /* スタック（最大要素数は100まで） */
+  unsigned int top;    /* スタックのトップの位置         */
 } Factorstack;
 
 void init_fstack();
 Factor fuctorpop();
-void fuctorpush(Factor x);
+void fuctorpush(char *, int, Scope);
 
 /* LLVMの関数定義 */
 typedef struct fundecl {
   char fname[256];      /* 関数名                      */
-  unsigned arity;       /* 引数個数                    */ 
+  unsigned arity;       /* 引数個数                    */
   Factor args[10];      /* 引数名                      */
   LLVMcode *codes;      /* 命令列の線形リストへのポインタ */
   struct fundecl *next; /* 次の関数定義へのポインタ      */
 } Fundecl;
 
-void add_code(LLVMCode* tmp);
+void add_code(LLVMcode *tmp);
 
 #endif
